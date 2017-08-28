42bbe38d7c2a3b83366a5ecd984031e98b9da312
AlbertDeng <frogdeng0319@gmail.con>
2017-08-28 20:23:51 +0800

    edit index.html

--- public/js/ui.js
+++ public/js/ui.js
@@ -5,7 +5,17 @@ $(function(){
 
 
 
+$('.search_btn').click(function(){
+		$('.magnifier').toggleClass( "w_flexible" )
+  // 		if($(this).hasClass("open")){
+		// 	$(this).removeClass("open");
+		// 	$(this).parent().find('.display_1').slideUp(200);
+		// } else{
+		// 	$(this).addClass("open");
+		// 	$(this).parent().find('.display_1').slideDown(300);
+		// }
 
+	});
 
 
 
