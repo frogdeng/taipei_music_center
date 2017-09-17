5a649b9e869e05fa35a4c23b7c953ef689965456
albertdeng <albertdeng@kkbox.com>
2017-09-18 00:50:10 +0800

    added others pages

--- public/js/ui.js
+++ public/js/ui.js
@@ -19,8 +19,37 @@ $('.search_btn').click(function(){
 
 
 
+  $(".hamburg").click(function(){
+     $('.aside').delay(200).toggleClass("hamburg-open");
+     $(this).delay(200).toggleClass("active");
+    });
 
+  $(".hamburg").click(function(){
+      $('.m-second_bg').removeClass("second-open");
 
+  	});
+
+   $(".second-goback").click(function(){
+      $('.m-second_bg').removeClass("second-open");
+      
+    });
+
+   
+	  $(".show-1").click(function(){
+	      $('.active-1').addClass("second-open");
+	    });
+	  $(".show-2").click(function(){
+	      $('.active-2').addClass("second-open");
+	    });
+	  $(".show-3").click(function(){
+	      $('.active-3').addClass("second-open");
+	    });
+	  $(".show-4").click(function(){
+	      $('.active-4').addClass("second-open");
+	    });	  
+	  $(".show-5").click(function(){
+	      $('.active-5').addClass("second-open");
+	    });
 
 
 });
